eeaffb30eedbd61f0c622ca0db68261466489da9
LGou-full <legiostridesign@gmail.com>
2024-09-07 22:19:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-container').addEventListener('click', function() {
+  document.querySelector('.sidebar').classList.toggle('active');
+  document.querySelector('.menu-list').classList.toggle('active');
+});
